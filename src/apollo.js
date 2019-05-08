8cce589d826df2da9ee05385300dbbcfb2ecefb1
ppark2ya <ppark2ya@naver.com>
2019-05-08 14:58:22 +0900

    basic set up

---
+++ src/apollo.js
@@ -0,0 +1,22 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink } from 'apollo-link';
+import { typeDefs, defaults, resolvers } from './clinetStats';
+
+const cache = new InMemoryCache();
+
+// Client State에 앱에 필요한 모든 로직을 넣어준다
+const stateLink = withClientState({
+    cache,
+    typeDefs, // 쿼리의 데이터 타입 정의
+    defaults,
+    resolvers // 실제 쿼리 요청이 들어왔을 때 수행하는 로직
+});
+
+const client = new ApolloClient({
+    cache,
+    link: ApolloLink.from([stateLink])
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+import GlobalStyle from './globalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Fragment>
+            <GlobalStyle />
+            <App />
+        </Fragment>
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
