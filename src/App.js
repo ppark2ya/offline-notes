3bd20f055c68e49411b0bbbcaa6b1815a22a1a0f
ppark2ya <vigabhan1231@gmail.com>
2019-05-11 15:08:46 +0900

    fragment, queries updaing..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import { GET_NOTES } from './queries';
 
 function App() {
   return (
     <div className="App">
+      <Query query={GET_NOTES}>{() => null}</Query>
     </div>
   );
 }
--- src/clinetStats.js
+++ src/clinetStats.js
@@ -1,3 +1,71 @@
-export const resolvers = {};
-export const defaults = {};
-export const typeDefs = {};
+import { NOTE_FRAGMENT } from './fragments';
+import { GET_NOTES } from './queries';
+
+const notes = [
+    {
+        id: 0,
+        title: 'title',
+        content: 'apollo',
+        __typename: 'Note'
+    }
+];
+
+export const defaults = {
+    notes
+};
+
+export const typeDefs = [
+    `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation {
+        createNote(title: String!, content: String!): Note
+        editNote(id: Int!, title: String!, content: String!): Note
+    }
+    type Note {
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
+
+export const resolvers = {
+    Query: {
+        note: (_, variables, { cache }) => {
+            const id = cache.config.dataIdFromObject({
+                __typename: "Note",
+                id: variables.id
+            });
+
+            const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id });
+            return note;
+        }
+    },
+    Mutation: {
+        createNote: (_, variables, { cache }) => {
+            const { notes } = cache.readQuery({ query: GET_NOTES });
+            const { title, content } = variables;
+
+            const newNote = {
+                __typename: "Note",
+                title,
+                content,
+                id: notes.length + 1
+            };
+
+            cache.writeData({
+                data: {
+                    notes: [newNote, ...notes]
+                }
+            });
+            return newNote;
+        }
+    }
+};
